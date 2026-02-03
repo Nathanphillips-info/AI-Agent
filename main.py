5c6d78e66a33902e98220c89c7e4d66b85120f07
Nathan <nathan.phillips1234@outlook.com>
2026-02-02 20:53:10 -0700

    added ai responses

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
+import os, argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types 
 
 def main():
     load_dotenv()
@@ -8,19 +9,28 @@ def main():
     if not api_key:
         raise RuntimeError("GEMINI_API_KEY environment variable not set")
     
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
         model='gemini-2.5-flash',
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+        contents= messages
         )
 
     usage = response.usage_metadata
     if usage is None:
         raise RuntimeError("GEMINI_API_KEY failed to run")
-    
-    print(f"Prompt Tokens: {usage.prompt_token_count}")
-    print(f"Usage Tokens: {usage.candidates_token_count}")
-    print(response.text)
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {usage.prompt_token_count}")
+        print(f"Response tokens: {usage.candidates_token_count}")
+        print(response.text)
+    else:
+        print(response.text)
 
 if __name__ == "__main__":
     main()
