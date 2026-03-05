a9a466ee13ea1bf99d6737970f08144edf11b575
Nathan <nathan.phillips1234@outlook.com>
2026-03-05 07:29:53 -0700

    added file writing function

---
+++ functions/write_file.py
@@ -0,0 +1,23 @@
+import os
+
+
+def write_file(working_directory, file_path, content):
+    try:
+        abs_working_dir = os.path.abspath(working_directory)
+        abs_file_path = os.path.normpath(os.path.join(abs_working_dir, file_path))
+        if os.path.commonpath([abs_working_dir, abs_file_path]) != abs_working_dir:
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+        
+        
+        parent_path = os.path.dirname(abs_file_path)
+        os.makedirs(parent_path, exist_ok=True)
+        
+        if os.path.isdir(abs_file_path):
+            f'Error: Cannot write to "{file_path}" as it is a directory'
+        
+        with open(abs_file_path, "w") as f:
+            f.write(content)
+            return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+    except Exception as e:
+        return f'Error Writing to file:{e}'
+    
